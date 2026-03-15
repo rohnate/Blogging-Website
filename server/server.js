882c1506634311bf71a21d9871b46c7f6235bfc7
itsRss <Rohitshekhawatwork@gmail.com>
2026-03-16 00:55:58 +0530

    setup basic things

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+// Load environment variables from .env file
+dotenv.config();
+
+// connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors()); // Allows React (port 3000) to talk to this server (port 5000)
+app.use(express.json()); // Allows server to read JSON data from requests
+
+// Test route
+app.get("/", (req, resp) => {
+  resp.send("Blog API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
